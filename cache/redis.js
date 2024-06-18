9510f6473d4ce8a655d5bfa77f655ab15a57809e
boyyveedo <talktoboyveedo@gmail.com>
2024-06-18 21:58:21 +0100

    last commit

--- cache/redis.js
+++ cache/redis.js
@@ -2,7 +2,7 @@ const redis = require('redis')
 
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST || localhost,
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT || 6379,
 })
 
