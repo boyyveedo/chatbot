41e8b1c79576b93862650802a6c3d1afe0f67569
boyyveedo <talktoboyveedo@gmail.com>
2024-06-18 21:39:09 +0100

    newest

--- cache/redis.js
+++ cache/redis.js
@@ -2,8 +2,8 @@ const redis = require('redis')
 
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST || localhost,
+    port: process.env.REDIS_PORT || 6379,
 })
 
 redisClient.on("connect", () => {
